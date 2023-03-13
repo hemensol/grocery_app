3f7733a8bc4998ed1aafb4e9359bc7a38d6abfdd
hemensol <hemensol14@yahoo.com>
2023-03-13 10:09:27 +0300

    last commit

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hemenzewdu/Documents/MobileApp/grocery_app/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/hemenzewdu/Documents/MobileApp/grocery_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
